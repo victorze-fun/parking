876fb04c9ba17e9f14105b2e9f817cf2851f53f0
Victor Zevallos <victorze@protonmail.com>
2022-05-11 17:17:19 -0500

    Add entry template

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, request, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return render_template('hello.html')
+@app.route('/')
+def home():
+    return 'Home'
+
+@app.route('/entry', methods=['POST', 'GET'])
+def entry():
+    if request.method == 'POST':
+        return redirect(url_for('home'))
+    return render_template('entry.html')
